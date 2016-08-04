57c554890f6f97d6f13eaced2cec07e5ec51c689
Tobias Kienzler <zommuter@users.noreply.github.com>
2016-08-04 16:20:51 +0200

    I like sha256 better...

--- hash2identicon.js
+++ hash2identicon.js
@@ -1,7 +1,7 @@
 var input = process.argv.slice(2).join(' ')
 
 // https://stackoverflow.com/questions/5878682/node-js-hash-string#comment15839272_11869589
-var hash = require('crypto').createHash('md5').update(input).digest("hex")
+var hash = require('crypto').createHash('sha256').update(input).digest("hex")
 
 // https://jdenticon.com
 var jdenticon = require("jdenticon"),
