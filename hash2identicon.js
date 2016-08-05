77d964e629a8927d837593f4233011b082678339
Tobias Kienzler <zommuter@users.noreply.github.com>
2016-08-05 11:20:09 +0200

    Crop to quarter for ico size ≤ 64x64

--- hash2identicon.js
+++ hash2identicon.js
@@ -12,4 +12,11 @@ var jdenticon = require("jdenticon"),
 fs.writeFileSync("./" + input + ".svg", svg);
 
 // https://stackoverflow.com/a/34958537/321973
-require('child_process').execSync('convert -background transparent "' + input + '".svg -define icon:auto-resize=16,32,48,64,256 "' + input + '".ico')
+require('child_process').execSync('convert -background transparent "' + input + '".svg \
+    \( -clone 0 -crop 50%x+0+0 -resize 16x16 \) \
+    \( -clone 0 -crop 50%x+0+0 -resize 32x32 \) \
+    \( -clone 0 -crop 50%x+0+0 -resize 48x48 \) \
+    \( -clone 0 -crop 50%x+0+0 -resize 64x64 \) \
+    \( -clone 0 -resize 128x128 \) \
+    \( -clone 0 -resize 256x256 \) \
+    -delete 0 "' + input + '".ico')
